9d57b8bb9bbd7a07886d1771e09189022a0c61d7
Mahnoor Fatima <Mahnoor@Aqeels-Mac.local>
2018-03-26 20:42:03 +0500

    Initial Commit

---
+++ iPay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPay
+//
+//  Created by Mahnoor Fatima on 3/26/18.
+//  Copyright © 2018 Mahnoor Fatima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
